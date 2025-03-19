e47c0a35574d2de76e69997680242b9035aaa0be
Md Sohid Ullah Chowdhury <mdsohidullahchowdhury1@gmail.com>
2025-03-19 21:57:20 +0600

    Adding App Screenshort in redme file

--- lib/View/cart.dart
+++ lib/View/cart.dart
@@ -14,84 +14,86 @@ class _CartState extends State<Cart> {
   Widget build(BuildContext context) {
     final cart = context.watch<CartProvider>().cart;
 
-    return Scaffold(
-      backgroundColor: const Color(0xffE1E1E1),
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        centerTitle: true,
-        title: const Text(
-          'Cart Items',
-          style: TextStyle(
-              fontWeight: FontWeight.w200, letterSpacing: 1.5, fontSize: 23),
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: const Color(0xffE1E1E1),
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          centerTitle: true,
+          title: const Text(
+            'Cart Items',
+            style: TextStyle(
+                fontWeight: FontWeight.w200, letterSpacing: 1.5, fontSize: 23),
+          ),
         ),
-      ),
-      body: ListView.builder(
-          itemCount: cart.length,
-          itemBuilder: (context, index) {
-            final cartItems = cart[index];
-
-            return ListTile(
-                leading: SizedBox(
-                  height: 180,
-                  width: 70,
-                  child: Image(
-                    image: AssetImage(cartItems['imageUrl'] as String),
-                    fit: BoxFit.fitHeight,
+        body: ListView.builder(
+            itemCount: cart.length,
+            itemBuilder: (context, index) {
+              final cartItems = cart[index];
+      
+              return ListTile(
+                  leading: SizedBox(
+                    height: 180,
+                    width: 70,
+                    child: Image(
+                      image: AssetImage(cartItems['imageUrl'] as String),
+                      fit: BoxFit.fitHeight,
+                    ),
                   ),
-                ),
-                title: Text(
-                  cartItems['title'].toString(),
-                  style: const TextStyle(
-                      fontSize: 20,
-                      letterSpacing: 1.2,
-                      fontWeight: FontWeight.bold),
-                ),
-                subtitle: Text(
-                  'Size: ${cartItems['sizes']}',
-                  style: const TextStyle(
-                      fontSize: 14, fontWeight: FontWeight.bold),
-                ),
-                trailing: IconButton(
-                    onPressed: () {
-                      showDialog(
-                        context: context,
-                        builder: (context) {
-                          return AlertDialog(
-                            title: const Text('Delete Item'),
-                            content: const Text(
-                                'Are You Sure You Wanna Delete the Item?'),
-                            actions: [
-                              TextButton(
-                                  onPressed: () {
-                                    Navigator.of(context).pop();
-                                  },
-                                  child: const Text(
-                                    'NO',
-                                    style: TextStyle(color: Colors.blue),
-                                  )),
-                              TextButton(
-                                  onPressed: () {
-                                    Provider.of<CartProvider>(context,
-                                            listen: false)
-                                        .deleteFromtheCart(cartItems);
-                                    Navigator.of(context).pop();
-                                  },
-                                  child: const Text(
-                                    'YES',
-                                    style: TextStyle(color: Colors.red),
-                                  ))
-                            ],
-                          );
-                        },
-                      );
-                    },
-                    icon: const Icon(
-                      Icons.delete,
-                      size: 25,
-                      color: Colors.red,
-                    )));
-          }),
+                  title: Text(
+                    cartItems['title'].toString(),
+                    style: const TextStyle(
+                        fontSize: 20,
+                        letterSpacing: 1.2,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  subtitle: Text(
+                    'Size: ${cartItems['sizes']}',
+                    style: const TextStyle(
+                        fontSize: 14, fontWeight: FontWeight.bold),
+                  ),
+                  trailing: IconButton(
+                      onPressed: () {
+                        showDialog(
+                          context: context,
+                          builder: (context) {
+                            return AlertDialog(
+                              title: const Text('Delete Item'),
+                              content: const Text(
+                                  'Are You Sure You Wanna Delete the Item?'),
+                              actions: [
+                                TextButton(
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                    child: const Text(
+                                      'NO',
+                                      style: TextStyle(color: Colors.blue),
+                                    )),
+                                TextButton(
+                                    onPressed: () {
+                                      Provider.of<CartProvider>(context,
+                                              listen: false)
+                                          .deleteFromtheCart(cartItems);
+                                      Navigator.of(context).pop();
+                                    },
+                                    child: const Text(
+                                      'YES',
+                                      style: TextStyle(color: Colors.red),
+                                    ))
+                              ],
+                            );
+                          },
+                        );
+                      },
+                      icon: const Icon(
+                        Icons.delete,
+                        size: 25,
+                        color: Colors.red,
+                      )));
+            }),
+      ),
     );
   }
 }
